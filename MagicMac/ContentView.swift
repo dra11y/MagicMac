5109322c7d9812dd2e6450c4ed53044d096d1847
Tom Grushka <tom@grushka.com>
2022-08-12 21:32:10 -0600

    Initial Commit

---
+++ MagicMac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MagicMac
+//
+//  Created by Tom Grushka on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MagicMac/MagicMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicMacApp.swift
+//  MagicMac
+//
+//  Created by Tom Grushka on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MagicMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
